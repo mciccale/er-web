470ae0a9acdbb5e2e71899838c804ca3f8e102f6
mciccale <marcociccalebaztan@gmail.com>
2022-05-11 15:48:16 +0200

    eliminated unused code

--- index.php
+++ index.php
@@ -5,17 +5,7 @@
 <div class="container p-4">
 
   <div class="row">
-
     <div class="col-md-4">
-
-      <?php if(isset($_SESSION["message"])){ ?>
-        <div class="alert alert-<?= $_SESSION['message-type'];?>
-        alert-dismissible fade show" role="alert">
-        <?= $_SESSION["message"] ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      <?php session_unset();} ?>
-
       <div class="card card-body">
         <form action="search_boss.php" method="POST">
           <div class="form-group">
@@ -26,21 +16,6 @@
           name="search_boss" value="Search">
         </form>
       </div>
-    </div>
-    <!-- <div class="col-md-8">
-
-        <table class="table tablebordered">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table> -->
     </div>
   </div>
 </div>
